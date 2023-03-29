8e3fa11ae4cc325bc9bdb9d2fd673a13cead02aa
Esther Verreau <everreau@ucop.edu>
2023-03-29 11:24:11 -0500

    remove unused

--- NAFAN/urls.py
+++ NAFAN/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from NAFAN import views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url
 
 urlpatterns = [
     path("", views.home, name="home"),
